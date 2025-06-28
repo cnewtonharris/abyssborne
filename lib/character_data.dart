2d38e6382afb88d510e27655eb35bb5c66886643
Colin Newton-Harris <col1695@hotmail.com>
2025-06-28 21:20:15 +0100

    Update character_data.dart
    added all methods, added factory for each class

--- lib/character_data.dart
+++ lib/character_data.dart
@@ -1,15 +1,16 @@
 class CharacterData {
   final String name;
   final String classType  ;
-  final int health;
-  final int maxHealth;
-  final int mana;
-  final int maxMana;
-  final int attack;
-  final int defense;
-  final int xp;
-  final int level;
-  final int gold;
+  int health;
+  int maxHealth;
+  int mana;
+  int maxMana;
+  int attack;
+  int defense;
+  int xp;
+  int xpToNextLevel;
+  int level;
+  int gold;
 
   CharacterData({
     this.name = '',
@@ -21,7 +22,94 @@ class CharacterData {
     this.attack = 10,
     this.defense = 5,
     this.xp = 0,
+    this.xpToNextLevel = 100,
     this.level = 1,
     this.gold = 0,
   });
+
+  //methods
+  bool get isAlive => health > 0;
+
+  double get healthPercentage => (health / maxHealth).clamp(0.0, 1.0);
+
+  void takeDamage(int damage) {
+    health -= damage;
+    if (health < 0) {
+      health = 0;
+    }
+  }
+
+  void spendMana(int amount) {
+    mana -= amount;
+    if (mana < 0) {
+      mana = 0;
+    }
+  }
+
+  void restoreMana(int amount) {
+    mana += amount;
+    if (mana > maxMana) {
+      mana = maxMana;
+    }
+  }
+
+  void gainXp(int amount) {
+    xp += amount;
+    if (xp >= 100) {
+      level += 1;
+      xp -= 100;
+    }
+  }
+
+  void nextXpLevelIncrease() {
+    xpToNextLevel = xpToNextLevel * 120 % 100;
+  }
+
+  factory CharacterData.warrior(String name) => CharacterData(
+    name: name,
+    classType: 'Warrior',
+    health: 100,
+    maxHealth: 100,
+    mana: 50,
+    maxMana: 50,
+    attack: 10,
+    defense: 5,
+    xp: 0,
+    xpToNextLevel: 100,
+    level: 1,
+    gold: 0,
+  );
+
+  factory CharacterData.wizard(String name) => CharacterData(
+    name: name,
+    classType: 'Wizard',
+    health: 100,
+    maxHealth: 100,
+    mana: 50,
+    maxMana: 50,
+    attack: 10,
+    defense: 5,
+    xp: 0,
+    xpToNextLevel: 100,
+    level: 1,
+    gold: 0,
+  );
+
+  factory CharacterData.archer(String name) => CharacterData(
+    name: name,
+    classType: 'Archer',
+    health: 100,
+    maxHealth: 100,
+    mana: 50,
+    maxMana: 50,
+    attack: 10,
+    defense: 5,
+    xp: 0,
+    xpToNextLevel: 100,
+    level: 1,
+    gold: 0,
+  );
 }
+
+
+
